f23e70643b38d537a1496555b52231b259954c20
lautaroVal <valenzuelalautaro@yahoo.com>
2023-04-08 11:00:25 -0300

    eliminar paciente listo

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   const [pacientes, setPacientes] = useState([])
   const [paciente, setPaciente] = useState({})
 
+  const eliminarPaciente = id => {
+    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id)
+
+    setPacientes(pacientesActualizados)
+  }
+
   return (
     <div className="container mx-auto mt-20">
       <Header />
@@ -20,6 +26,7 @@ function App() {
         <ListadoPaciente 
          pacientes={pacientes}
          setPaciente={setPaciente}
+         eliminarPaciente={eliminarPaciente}
         />
       </div>
     </div>
--- src/components/ListadoPaciente.jsx
+++ src/components/ListadoPaciente.jsx
@@ -1,6 +1,6 @@
-import Paciente from "./Paciente"
+import Paciente from "./Paciente";
 
-const ListadoPaciente = ({ pacientes, setPaciente }) => {
+const ListadoPaciente = ({ pacientes, setPaciente, eliminarPaciente }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       {pacientes && pacientes.length ? (
@@ -16,21 +16,23 @@ const ListadoPaciente = ({ pacientes, setPaciente }) => {
           </p>
 
           {pacientes.map((paciente) => (
-            <Paciente key={paciente.id} paciente={paciente} setPaciente={setPaciente}/>
+            <Paciente
+              key={paciente.id}
+              paciente={paciente}
+              setPaciente={setPaciente}
+              eliminarPaciente={eliminarPaciente}
+            />
           ))}
         </>
       ) : (
         <>
-        <h2 className="font-black text-3xl text-center">
-            No hay pacientes
-          </h2>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
           <p className="text-xl mt-5 mb-10 text-center">
             Comienza agregando pacientes {""}
             <span className="text-indigo-600 font-bold ">
               y aparecerán en este lugar
             </span>
           </p>
-
         </>
       )}
     </div>
